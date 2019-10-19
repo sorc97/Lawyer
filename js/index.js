fef96a6d9dcf4d14d733b17a70f35039c9fe665a
sorc97 <nnd64809200@gmail.com>
2019-10-19 14:58:40 +0300

    Mobile menu functional

---
+++ js/index.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const mobileMenuButton = document.querySelector('.menu-icon');
+const menuItems = document.querySelectorAll('.menu-item');
+let isMenuActive = false;
+
+class mobileMenu {
+  constructor(items) {
+    
+    let ul = document.createElement('ul');
+    ul.className = 'mobileMenu';
+    ul.addEventListener('click', () => toggleMenu());
+    ul.innerHTML = '<li class="mobileMenu-closeButton">&times;</li>';
+    
+    const createLi = text => {
+      let li = document.createElement('li');
+      li.innerHTML = text;
+      li.className = 'mobileMenu-item';
+      return li;
+    }
+
+    items.forEach(item => ul.append(createLi(item.innerHTML)));
+
+    return ul;
+  }
+}
+
+function toggleMenu() {
+  let menu = document.querySelector('.mobileMenu');
+
+  menu.style.display = (isMenuActive) ? 'none' : 'flex';
+  isMenuActive = !isMenuActive;
+}
+
+mobileMenuButton.addEventListener('click', () => {
+  let menu = document.querySelector('.mobileMenu');
+
+  if(menu) {
+    toggleMenu();
+    return;
+  }
+
+  mobileMenuButton.after(new mobileMenu(menuItems));
+  isMenuActive = true;
+})
