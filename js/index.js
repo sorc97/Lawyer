763126321097cbd69041884d95bea4fb61bd42c8
sorc97 <nnd64809200@gmail.com>
2020-01-31 15:38:26 +0300

    production

--- js/index.js
+++ js/index.js
@@ -1,50 +1,25 @@
 'use strict'
 
-const mobileMenuToggler = document.querySelector('.menu-icon');
+const menuToggler = document.querySelector('.menu-icon');
+const menu = document.querySelector('.navigation');
 const menuItems = document.querySelectorAll('.menu-item');
-let isMenuActive = false;
+const closeMenuButton = document.querySelector('.menu-closeButton');
 
-// Menu Component
-class MobileMenu {
-  constructor(items) {
-    //Menu wrapper
-    let menuWrapper = document.createElement('div');
-    menuWrapper.className = 'mobileMenu';
-    menuWrapper.innerHTML = '<span class="mobileMenu-closeButton">&times;</span>';
-    menuWrapper.addEventListener('click', () => toggleMenu());
-    //Menu list
-    let menuList = document.createElement('ul');
-    menuList.className = 'mobileMenu-list';
-    menuWrapper.append(menuList);
-    
-    const createLi = text => {
-      let li = document.createElement('li');
-      li.innerHTML = text;
-      li.className = 'mobileMenu-item';
-      return li;
-    }
+function menuClose() {
+  menu.classList.remove('open');
 
-    items.forEach(item => menuList.append(createLi(item.innerHTML)));
-
-    return menuWrapper;
-  }
+  menuItems.forEach(
+    item => item.removeEventListener('click', menuClose)
+  )
 }
 
-function toggleMenu() {
-  let menu = document.querySelector('.mobileMenu');
-
-  menu.style.display = (isMenuActive) ? 'none' : 'flex';
-  isMenuActive = !isMenuActive;
+function menuOpen() {
+  menu.classList.add('open');
+  
+  menuItems.forEach(
+    item => item.addEventListener('click', menuClose)
+  )
 }
 
-mobileMenuToggler.addEventListener('click', () => {
-  let menu = document.querySelector('.mobileMenu');
-
-  if(menu) {
-    toggleMenu();
-    return;
-  }
-
-  mobileMenuToggler.after(new MobileMenu(menuItems));
-  isMenuActive = true;
-})
+menuToggler.addEventListener('click', menuOpen);
+closeMenuButton.addEventListener('click', menuClose);
